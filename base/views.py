c70de342bedfbc5914396f2de242104948748519
Kariuki11 <irungukenneth433@gmail.com>
2024-07-09 20:02:22 +0300

    COMMITS

--- base/views.py
+++ base/views.py
@@ -50,4 +50,7 @@ def updateRoom(request, pk):
     return render(request, 'base/room_form.html', context)
 
 def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
     return render(request, 'base/delete.html', {'obj':room})
