f7808657744dc73842446f0771934d5a15f47868
Kariuki11 <irungukenneth433@gmail.com>
2024-07-10 02:38:27 +0300

    New changes

--- base/views.py
+++ base/views.py
@@ -16,7 +16,10 @@ def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     
     rooms = Room.objects.filter(
-        Q(topic__name__icontains=q))
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
     
     topics = Topic.objects.all()
     
