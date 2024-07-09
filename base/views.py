c4bb3a5fc6cd471232ac4104879c729fc2b6aeac
Kariuki11 <irungukenneth433@gmail.com>
2024-07-09 21:02:41 +0300

    New changes

--- base/views.py
+++ base/views.py
@@ -12,8 +12,9 @@ from .forms import RoomForm
 
 
 def home(request):
-    q = request.GET.get('q')
-    rooms = Room.objects.filter(topic__name=q)
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    
+    rooms = Room.objects.filter(topic__name__icontains=q)
     
     topics = Topic.objects.all()
     
