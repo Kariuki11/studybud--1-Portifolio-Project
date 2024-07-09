9863b99b250e962bb57b8edae3ff9a70506f187e
Kariuki11 <irungukenneth433@gmail.com>
2024-07-09 20:03:18 +0300

    New changes

--- base/views.py
+++ base/views.py
@@ -53,4 +53,5 @@ def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
     if request.method == 'POST':
         room.delete()
+        return redirect('home')
     return render(request, 'base/delete.html', {'obj':room})
