62fbff84acdd48acfab7417f5bd6555b4dc8aa6e
Kariuki11 <irungukenneth433@gmail.com>
2024-07-10 02:31:04 +0300

    New changes

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -14,7 +15,8 @@ from .forms import RoomForm
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q))
     
     topics = Topic.objects.all()
     
