f8effc74b42cebf4a0b864c829031569889aad1c
Kariuki11 <irungukenneth433@gmail.com>
2024-07-10 17:26:15 +0300

    New changes

--- base/views.py
+++ base/views.py
@@ -12,6 +12,11 @@ from .forms import RoomForm
 # ]
 
 
+def loginPage(request):
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     
